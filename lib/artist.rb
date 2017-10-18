b610544339e96649784ba4dd569957dd0e401aa6
Adam Fudge <coolfudge@gmail.com>
2017-10-18 18:04:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,4 +5,15 @@ class Artist
     @name = name
     @songs = []
   end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    @songs << song
+    song.artist = self
+  end
 end
