4e82329ea2620b40a29ef3ff1ac53937193e335f
Adam Fudge <coolfudge@gmail.com>
2017-10-18 18:20:22 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,9 +12,16 @@ class Author
   end
 
   def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
   def self.post_count
