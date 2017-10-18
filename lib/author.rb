6541f18210e31fe16aefa5b0083d9ac07dc7d813
Adam Fudge <coolfudge@gmail.com>
2017-10-18 18:18:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,23 @@
 class Author
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
     @posts = []
     @@post_count = 0
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+  end
+
+  def add_post_by_title(title)
+  end
+
+  def self.post_count
+    @@post_count
   end
 end
